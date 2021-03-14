d6be8ab5a13136b9f35bbe604a55be856ea5a266
Carlton Joseph <carlton.joseph@gmail.com>
2021-03-13 18:47:23 -0800

    add types to the compose api

--- src/composables/products.ts
+++ src/composables/products.ts
@@ -1,8 +1,26 @@
 import { ref } from "vue";
 import useApi from "./api";
 
+export interface Variant {
+  id: string;
+  title: string;
+  sku: string;
+  quantity: number;
+}
+
+export interface Product {
+  id: string;
+  title: string;
+  description: string;
+  images: string[];
+  category: string;
+  variants: Variant[];
+  price: number;
+  tags: string[];
+}
+
 export default async function useProducts() {
-  const { response: products, request } = useApi(
+  const { response: products, request } = useApi<Product[]>(
     "https://ecomm-products.modus.workers.dev/"
   );
 
